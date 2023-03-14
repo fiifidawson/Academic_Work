1f21618193efda48a4d137d9cfe4ce5cd20ccc61
Fiifi Dawson <edem.fiifi.dawson@gmail.com>
2023-03-14 18:53:27 +0000

    Update Code_2.asm

--- Computer_Architecture_Organisation/Code_2.asm
+++ Computer_Architecture_Organisation/Code_2.asm
@@ -12,4 +12,25 @@
 ;MOV DH, 24H 
 ;MOV BL, DH
 ;MOV CX, AX
-;
+ 
+;MOV AX, 0B800h; set AX to hexadecimal value of B800h
+;mov ds, ax; copy value of AX to DS
+;MOV CH, 1101_1111B; set CH to binary value
+;MOV BX, 15Eh; set BX to 15Eh 
+
+;MOV AX, 037AH
+;MOV DS, AX 
+;MOV CL, 1110_1100B
+;MOV BX, 15EH
+;MOV [BX], CX     
+
+
+;MOV AX, 047AH
+;MOV DS, AX
+;MOV CL, 1110_1101B
+;MOV BX, 15EH
+;MOV [BX], CX
+;MOV DL, [BX];Copy byte from memory at [BX] to DL
+
+
+MOV AX, 1A3AEH
